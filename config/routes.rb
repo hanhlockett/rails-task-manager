d257d6c766e5c637e1b2629bd282d8908b0e16a5
hanh lockett <honghanhbr2007@yahoo.com>
2023-11-13 11:42:38 +1100

    add task model

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/tasks', to: 'tasks#index'
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  # get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  get '/tasks/new', to: 'tasks#new'
+  post '/tasks', to: 'tasks#create'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  # patch '/tasks/:id', to: 'tasks#update'
+  patch '/tasks/:id', to: 'tasks#update', as: 'update_task'
+  delete '/tasks/:id', to: 'tasks#destroy', as: 'delete_task'
 end
