87b761739c9246a0764ee4185612855fa373d923
hanh lockett <honghanhbr2007@yahoo.com>
2023-11-13 17:06:01 +1100

    Rails Task Manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,38 +1,49 @@
 class TasksController < ApplicationController
     def index
-        @tasks = Task.all
-        
+      @tasks = Task.all
     end
-
+  
     def show
-        @task = Task.find(params[:id])
+      @task = Task.find(params[:id])
     end
-
+  
     def new
-        @task = Task.new
-        # raise
+      @task = Task.new
     end
-
+  
     def create
-        @task = Task.new(task_params)
-        @task.save
-        redirect_to task_path(@task)
+      @task = Task.new(task_params)
+      if @task.save
+        redirect_to @task
+      else
+        render 'new'
+      end
     end
-
+  
     def edit
-        @task = Task.find(params[:id])
+      @task = Task.find(params[:id])
+    end
+  
+    def update
+      @task = Task.find(params[:id])
+      if @task.update(task_params)
+        redirect_to @task
+      else
+        render 'edit'
+      end
     end
 
-    def update  
-        @task = Task.find(params[:id])
-        @task.update(task_params)
-        redirect_to task_path(@task)
-    end
-
+  
     def destroy
-        @task = Task.find(params[:id])
-        @task.destroy
-        redirect_to tasks_path
+      @task = Task.find(params[:id])
+      @task.destroy
+      redirect_to tasks_path, status: :see_other
     end
-
-end
+  
+    private
+  
+    def task_params
+      params.require(:task).permit(:title, :details, :completed)
+    end
+  end
+  
